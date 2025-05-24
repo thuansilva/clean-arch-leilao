2bfbf318aa5adf6f436d431738e14fe2bce9f0fb
Thuan Silva <thuan.aguiar@elloxdigital.com>
2025-05-23 20:41:20 -0400

    Adiciona validação de lances na classe Auction

--- src/Auction.ts
+++ src/Auction.ts
@@ -1,3 +1,5 @@
+import Bid from "./Bid";
+
 export default class Auction {
   constructor(
     public auctionId: string,
@@ -6,4 +8,19 @@ export default class Auction {
     public minIncrement: number,
     public startAmount: number
   ) {}
+
+  validateBid(bid: Bid, highestBid: Bid) {
+    if (bid.date.getTime() > this.endDate.getTime())
+      throw new Error("Auction is not open");
+
+    if (highestBid && highestBid.amount > bid.amount)
+      throw new Error("Bid amount must be greater than the last bid");
+
+    if (highestBid && highestBid.customer === bid.customer)
+      throw new Error(
+        "Auction does not accept sequencial bids from the same customer"
+      );
+
+    return true;
+  }
 }
